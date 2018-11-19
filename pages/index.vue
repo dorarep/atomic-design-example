af0ba3a4d4cc4ccf88df2646f82b711070c5a62d
dorarep <ishikami.naotaka@gmail.com>
2018-11-19 09:46:31 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  section.container
+</template>
+
+<script>
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
+
