af1f19d6b7185ffe8564f080dc36c2657b12f9fb
dorarep <ishikami.naotaka@gmail.com>
2019-07-31 17:05:42 +0900

    Add ellipse-text component.

---
+++ components/atoms/EllipseText/index.stories.js
@@ -0,0 +1,12 @@
+import { storiesOf } from '@storybook/vue'
+import EllipseText from './index.vue'
+
+storiesOf('EllipseText', module)
+  .add('はみ出る', () => ({
+    components: { EllipseText },
+    template: '<ellipse-text style="width: 100px; height: 90px" text="あいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえおあいうえお" />'
+  }))
+  .add('はみ出ない', () => ({
+    components: { EllipseText },
+    template: '<ellipse-text style="width: 100px; height: 90px" text="あいうえお" />'
+  }))
---
+++ components/atoms/EllipseText/index.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  .wrapper
+    div(ref="textArea") {{ shownText }}
+</template>
+
+<script>
+  export default {
+    props: {
+      text: String
+    },
+    data: () => ({ hiddenLetters: 0 }),
+    computed: {
+      shownText() {
+        return this.hiddenLetters > 0 ? this.text.slice(0, -this.hiddenLetters) + '...' : this.text
+      }
+    },
+    mounted() {
+      this.sliceOverflownText()
+    },
+    updated() {
+      this.sliceOverflownText()
+    },
+    methods: {
+      sliceOverflownText() {
+        if (this.$refs.textArea.clientHeight > 90) {
+          this.hiddenLetters++
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  .wrapper
+    overflow hidden
+</style>
